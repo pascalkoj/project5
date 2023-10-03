d4a61da795112161369756aac7d009ad0598772a
Koj, Pascal Marie <pkoj@iu.edu>
2023-10-03 19:42:34 -0400

    done done done doend oedn done

--- app/src/main/java/com/example/project5/MainActivity.kt
+++ app/src/main/java/com/example/project5/MainActivity.kt
@@ -21,40 +21,59 @@ import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.TranslatorOptions
 import com.example.project5.MainActivity.TranslatorViewModel
 import com.example.project5.databinding.ActivityMainBinding
+import com.example.project5.databinding.FragmentTextScreenBinding
+import com.google.mlkit.common.model.RemoteModelManager
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bindingFrag: FragmentTextScreenBinding
     //val viewModel: TranslatorViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bindingFrag = FragmentTextScreenBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-
-
         val viewModel = ViewModelProvider(this).get(TranslatorViewModel::class.java)
-        viewModel.SetTextViews(binding.typeHere, binding.lookAt)
+        //viewModel.SetOutputText(binding.lookAt)
+        val lookatview = binding.lookAt
+        val edittextview = bindingFrag.typeHere
+
+
+        edittextview.setOnClickListener {
+            val translated_str = viewModel.DoTranslateWithTextViews(edittextview.text.toString())
+            lookatview.setText(translated_str)
+        }
 
         binding.sourceLanguge.setOnCheckedChangeListener { group, checkedId ->
+            val viewModel = ViewModelProvider(this).get(TranslatorViewModel::class.java)
+
             when (checkedId) {
                 R.id.sEnglish -> viewModel.SetLanguages(TranslateLanguage.ENGLISH, viewModel.outputLanguageOption)
                 R.id.sSpanish -> viewModel.SetLanguages(TranslateLanguage.SPANISH, viewModel.outputLanguageOption)
                 R.id.sGerman -> viewModel.SetLanguages(TranslateLanguage.GERMAN, viewModel.outputLanguageOption)
             }
-            viewModel.DoTranslateWithTextViews()
+
+            val translated_str = viewModel.DoTranslateWithTextViews(edittextview.text.toString())
+            lookatview.setText(translated_str)
         }
 
         binding.Translation.setOnCheckedChangeListener { group, checkedId ->
+            val viewModel = ViewModelProvider(this).get(TranslatorViewModel::class.java)
+
             when (checkedId) {
                 R.id.tEnglish -> viewModel.SetLanguages(viewModel.inputLanguageOption, TranslateLanguage.ENGLISH)
                 R.id.tSpanish -> viewModel.SetLanguages(viewModel.inputLanguageOption, TranslateLanguage.SPANISH)
                 R.id.tGerman -> viewModel.SetLanguages(viewModel.inputLanguageOption, TranslateLanguage.GERMAN)
             }
-            viewModel.DoTranslateWithTextViews()
+
+            val translated_str = viewModel.DoTranslateWithTextViews(edittextview.text.toString())
+            lookatview.setText(translated_str)
+
         }
 
     }
@@ -68,15 +87,6 @@ class MainActivity : AppCompatActivity() {
         var inputLanguageOption = TranslateLanguage.ENGLISH
         var outputLanguageOption = TranslateLanguage.GERMAN
 
-        var inputText: EditText? = null
-        var outputTextView: TextView? = null
-
-        // called on init. Caches references to the input/output text boxes
-        fun SetTextViews(inputText: EditText, outputTextView: TextView)
-        {
-            this.inputText = inputText
-            this.outputTextView = outputTextView
-        }
         // call when user changes languages option
         fun SetLanguages(inputLanguageCode: String, outputLanguageCode: String)
         {
@@ -84,14 +94,8 @@ class MainActivity : AppCompatActivity() {
             this.outputLanguageOption = outputLanguageCode
         }
 
-
-        fun DoTranslateWithTextViews()
+        fun DoTranslateWithTextViews(input_str: String) : String
         {
-            if (inputText == null || outputTextView == null)
-            {
-                return;
-            }
-            var input_str = inputText?.text.toString()
             var translated_str = ""
             if (inputLanguageOption == "")
             {
@@ -101,7 +105,7 @@ class MainActivity : AppCompatActivity() {
             {
                 translated_str = TranslateWithLang(input_str, inputLanguageOption, outputLanguageOption)
             }
-            outputTextView?.setText(translated_str)
+            return translated_str
         }
 
         // output language code is something like TranslateLanguage.ENGLISH
@@ -141,30 +145,13 @@ class MainActivity : AppCompatActivity() {
             var conditions = DownloadConditions.Builder()
                 .requireWifi()
                 .build()
-            translator.downloadModelIfNeeded(conditions)
-                .addOnSuccessListener {
-                    // Model downloaded successfully. Okay to start translating.
-                    // (Set a flag, unhide the translation UI, etc.)
-                    translator.translate(text)
-                        .addOnSuccessListener { translatedText ->
-                            // Translation successful.
-                            println(translatedText)
-                            output = translatedText
-                        }
-                        .addOnFailureListener { exception ->
-                            // Error.
-                            // ...
-                            Log.e("ERROR",
-                                "Failed to translate: $text  from $inputLanguageCode to $outputLanguageCode"
-                            )
-                        }
-                }
-                .addOnFailureListener { exception ->
-                    // Model couldn’t be downloaded or other internal error.
-                    // ...
-                    Log.e("ERROR", "Model $inputLanguageCode -> $outputLanguageCode couldn't be downloaded")
-                }
-            return output
+            var task = translator.downloadModelIfNeeded(conditions)
+            while (!task.isComplete)
+            { }
+            var task2 = translator.translate(text)
+            while (!task2.isComplete)
+            { }
+            return task2.result
         }
 
     }
--- app/src/main/java/com/example/project5/textScreen.kt
+++ app/src/main/java/com/example/project5/textScreen.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.project5.databinding.ActivityMainBinding
+import com.example.project5.databinding.FragmentTextScreenBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +24,18 @@ class textScreen : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentTextScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        binding = FragmentTextScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        val viewModel = ViewModelProvider(this).get(MainActivity.TranslatorViewModel::class.java)
+
     }
 
     override fun onCreateView(
